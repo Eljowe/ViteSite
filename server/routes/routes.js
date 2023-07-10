602e2a135c90270fcfa86abbdfeebd564c572a3f
Eljowe <98017040+Eljowe@users.noreply.github.com>
2023-07-10 12:31:32 +0300

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -20,25 +20,5 @@ router.route('/2').get((req, res) => {
   res.status(200).json({ message: "Hello from API 2" })
 })
 
-router.route('/').post(async (req, res) => {
-  try {
-    const { prompt } = req.body;
-
-    const response = await openai.createImage({
-      prompt,
-      n: 1,
-      size: '1024x1024',
-      response_format: 'b64_json'
-    });
-
-    const image = response.data.data[0].b64_json;
-    console.log(image)
-
-    res.status(200).json({ photo: image });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Something went wrong" })
-  }
-})
 
 export default router;
